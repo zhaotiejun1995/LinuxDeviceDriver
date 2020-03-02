7c03896c6517fc46900cf14351d254c38d3e09d7
zhaotiejun1995 <49424535+zhaotiejun1995@users.noreply.github.com>
2020-03-02 13:41:54 +0800

    Create signal_test.c

---
+++ S3C2440/ 05_signal/signal_test.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <poll.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int fd;
+
+void signal_fun(int signum)
+{
+    unsigned char key_val;
+    read(fd, &key_val, 1);
+    printf("key_val: 0x%x\n", key_val);
+}
+
+int main(int argc, char **argv)
+{
+	unsigned char key_val;
+	int ret;
+    int oflags;
+    signal(SIGIO, signal_fun);
+    
+	fd = open("/dev/signal", O_RDWR);
+	if (fd < 0)
+	{
+		printf("can't open!\n");
+	}
+
+	fcntl(fd, F_SETOWN, getpid()); //告诉内核
+	oflags = fcntl(fd, F_GETFL);
+	fcntl(fd, F_SETFL, oflags | FASYNC);//改变fasync，会调用驱动中的fasync函数指针对应的函数。
+
+	while (1)
+	{   
+	    sleep(1000);
+	}
+	return 0;
+}
+
+
